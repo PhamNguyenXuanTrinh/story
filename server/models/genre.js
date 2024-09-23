880bc67109a26d80fbe7e60249f305b312df8478
Trinh Pham <pnxtrinh@gmail.com>
2024-09-23 14:16:05 +0700

    fix pre save name genre on db

--- server/models/genre.js
+++ server/models/genre.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
 
+// Function to capitalize the first letter of each word
+const capitalizeWords = (str) => {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+};
+
 const genreSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  slug: { type: String},
+  slug: { type: String },
   description: { type: String },
-  story: [{type: mongoose.Types.ObjectId, ref: "Story"}]
+  story: [{ type: mongoose.Types.ObjectId, ref: "Story" }]
+});
+
+// Pre-save hook to capitalize the name field
+genreSchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.name = capitalizeWords(this.name); // Capitalize the first letter of each word
+  }
+  next();
 });
 
 module.exports = mongoose.model("Genre", genreSchema);
